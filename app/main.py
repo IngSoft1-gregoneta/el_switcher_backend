91d116e678bf36151a6f3fbb1e8c36b01c655817
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2024-09-18 11:21:14 -0300

    SES-11 fixed wronly pulled main

--- app/main.py
+++ app/main.py
@@ -49,12 +49,12 @@ def get_id():
           response_model=RoomOut,
           status_code=status.HTTP_201_CREATED)
 async def create_room(new_room: RoomIn) -> RoomOut:
+    if new_room.players_expected < 2 or new_room.players_expected > 4:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong amount of players")
     for room in ROOMS:
         if room["room_name"] == new_room.room_name:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room name already exists")
-        if new_room.players_expected < 2 or new_room.players_expected > 4:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong amount of players")
-
+    
     try:
         last_id = ROOMS[-1]['room_id'] if ROOMS else 0
         new_id = last_id + 1
@@ -63,16 +63,18 @@ async def create_room(new_room: RoomIn) -> RoomOut:
         roomOut = RoomOut(room_id=new_id,
                           room_name=new_room.room_name,
                           players_expected=new_room.players_expected,
-                          owner_name=new_room.owner_name)
+                          players_names=[new_room.owner_name],
+                          owner_name=new_room.owner_name,
+                          is_active=True)
     
         ROOMS.append(roomOut.model_dump())
-        
-        await manager.broadcast("Game created")        
+       
         return roomOut.model_dump()
-    
+
     except Exception as e:
         print(f"Error: {e}")  # Debug error
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+     HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
 
 # Define endpoint to get rooms list
 @app.get("/rooms/")
