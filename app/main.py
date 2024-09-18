aad8628c56e711619426dc768d35a309a7e5cf0f
tadeofiorini <tadeo.nino.039@mi.unc.edu.ar>
2024-09-18 00:13:40 -0300

    SES-7_create_room

--- app/main.py
+++ app/main.py
@@ -29,12 +29,12 @@ def get_id():
           response_model=RoomOut,
           status_code=status.HTTP_201_CREATED)
 async def create_room(new_room: RoomIn) -> RoomOut:
+    if new_room.players_expected < 2 or new_room.players_expected > 4:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong amount of players")
     for room in ROOMS:
         if room["room_name"] == new_room.room_name:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room name already exists")
-        if new_room.players_expected < 2 or new_room.players_expected > 4:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong amount of players")
-
+    
     try:
         last_id = ROOMS[-1]['room_id'] if ROOMS else 0
         new_id = last_id + 1
@@ -55,52 +55,3 @@ async def create_room(new_room: RoomIn) -> RoomOut:
         print(f"Error: {e}")  # Debug error
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
     
-# Define endpoint to create a new room
-@app.get("/rooms/")
-async def get_rooms():
-    try:
-        return ROOMS
-    except Exception as e:
-        print(f"Error: {e}")  # Debug error
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
-
-# endpoint for join room
-@app.put("/rooms/join/")
-def join_room_endpoint(room_id: int, player_name: str):
-    try:
-        room = get_room_by_id(room_id)
-
-        if room is None:
-            return {"message": "Room not found"}
-
-        if len(room["players"]) == room["players_expected"]:
-            return {"message": "Room is full"}
-        
-        if player_name in room["players"]:
-            return {"message": "The name already exists, choose another"}
-        
-        room["players"].append(player_name)
-        return {"message": f"The player {player_name} has joined the room {room_id}"}
-    
-    except Exception as e:
-        print(f"Error: {e}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
-
-# endpoint for room join request 
-@app.put("/rooms/leave/")
-def leave_room_endpoint(room_id: int, player_name: str):
-    try:
-        room = get_room_by_id(room_id)
-
-        if room == None:
-            return {"message": "Room not found"}
-
-        if not(player_name in room["players"]):
-            return {"message": "There is not such a player"}
-        
-        room["players"].remove(player_name)
-        return {"message": f"The player {player_name} has left the room {room_id}"}
-    
-    except Exception as e:
-        print(f"Error: {e}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")    
