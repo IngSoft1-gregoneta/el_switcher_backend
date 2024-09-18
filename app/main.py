870f2c76619e6a61e2593095a90e3feaaee24457
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2024-09-18 02:21:16 -0300

    SES-30 prepared for fetching new structures

--- app/main.py
+++ app/main.py
@@ -22,42 +22,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define endline to create a new room
-@app.post("/rooms/",
-          response_model=room_model.RoomOut,
-          status_code=status.HTTP_201_CREATED)
-async def create_room(new_room: room_model.RoomIn) -> room_model.RoomOut:
-    for room in rooms.ROOMS:
-        if room["room_name"] == new_room.room_name:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room name already exists")
-    try:
-        last_id = rooms.ROOMS[-1]['room_id'] if rooms.ROOMS else 0
-        new_id = last_id + 1
-
-        # Create a new room dict
-        room_dict = {
-            "room_id": new_id,
-            "room_name": new_room.room_name,
-            "players_expected": new_room.players_expected,
-            "players": [],
-            "is_active": True,
-        }
-
-        rooms.ROOMS.append(room_dict)
-        
-        return room_model.RoomOut(**room_dict)
-    except Exception as e:
-        print(f"Error: {e}")  # Debug error
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
-# Define endpoint to create a new room
-@app.get("/rooms/")
-async def get_rooms():
-    try:
-        return rooms.ROOMS
-    except Exception as e:
-        print(f"Error: {e}")  # Debug error
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
-
 # Define endpoint to update match state with finished turn
 @app.put("/match/{room_id}/endturn/",
           response_model=room_model.RoomOut)
