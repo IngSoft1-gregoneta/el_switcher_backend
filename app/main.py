639edb03059429a74829225c29dfb7b2252555ca
tadeofiorini <tadeo.nino.039@mi.unc.edu.ar>
2024-09-17 22:18:39 -0300

    feature_SES-50_make_n_join_websockets_for_list_games into develop

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from typing import Union
-
-from fastapi import FastAPI
+# FastApi
+from uuid import uuid4
+from fastapi import Cookie, FastAPI, WebSocket, WebSocketDisconnect
+# Middleware to allow methods from react
 from fastapi.middleware.cors import CORSMiddleware
+# Default query parameters
+from typing import Annotated
+# Data from manager.py
+from manager import manager
 
 app = FastAPI()
 
@@ -15,22 +20,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+manager = manager.ConnectionManager()
+# Aca podriamos asignar el mismo socket para el grupo de jugadores en la misma partida?
+user_socket = {}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.websocket("/ws")
+async def websocket_endpoint(
+    websocket: WebSocket, user_id: Annotated[str | None, Cookie()] = None
+):
+   # logger.debug(user_id)
+    await manager.connect(websocket)
+    user_socket[user_id] = websocket
+    try:
+        while True:
+            data = await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        
+@app.get("/get_id")
+def get_id():
+    return uuid4()
 
-
-@app.get("/list_games")
-def list_games():
-    return [
-        {"id": 1, "name": "Game 1"},
-        {"id": 2, "name": "Game 2"},
-        {"id": 3, "name": "Game 3"},
-        {"id": 3, "name": "carade"},
-    ]
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
