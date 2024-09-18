74fc1607d26d875bfc7f75fe61b4456e7a9022a9
Braian Rodriguez <braianrodriguez4476@mi.unc.edu.ar>
2024-09-18 11:08:50 -0300

    SES-15_Endpoint_leave_room merged now succesfuly into develop

--- app/main.py
+++ app/main.py
@@ -70,8 +70,25 @@ async def create_room(new_room: RoomIn) -> RoomOut:
         ROOMS.append(roomOut.model_dump())
        
         return roomOut.model_dump()
-    
+
     except Exception as e:
         print(f"Error: {e}")  # Debug error
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
     
+# endpoint for room leave request 
+@app.put("/rooms/leave/")
+def leave_room_endpoint(room_id: int, player_name: str):
+    try:
+        room = get_room_by_id(room_id)
+
+        if room == None:
+            return {"message": "Room not found"}
+        if not(player_name in room["players_names"]):
+            return {"message": "There is not such a player"}
+        
+        room["players_names"].remove(player_name)
+        return {"message": f"The player {player_name} has left the room {room_id}"}
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
