75eb7b582e2c8f07daebe7fab626078806a393bb
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2024-09-17 21:08:53 -0300

    SES-11 Added common main structure and fix in GET endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
-from typing import Union
-
-from fastapi import FastAPI, HTTPException, status
-from room import rooms
+# FastApi
+from uuid import uuid4
+from fastapi import Cookie, FastAPI, HTTPException, WebSocket, WebSocketDisconnect, status
+# Middleware to allow methods from react
 from fastapi.middleware.cors import CORSMiddleware
+# data, methods and classes of a room
+from room import *
+# Default query parameters
+from typing import Annotated, Optional
+# Data from manager.py
+from manager import manager
 
 app = FastAPI()
 
@@ -16,12 +22,103 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+manager = manager.ConnectionManager()
+# Aca podriamos asignar el mismo socket para el grupo de jugadores en la misma partida?
+user_socket = {}
 
-# Define endpoint to create a new room
-@app.get("/rooms/")
-async def get_rooms():
+@app.websocket("/ws")
+async def websocket_endpoint(
+    websocket: WebSocket, user_id: Annotated[str | None, Cookie()] = None
+):
+   # logger.debug(user_id)
+    await manager.connect(websocket)
+    user_socket[user_id] = websocket
     try:
-        return rooms.ROOMS
+        while True:
+            data = await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        
+@app.get("/get_id")
+def get_id():
+    return uuid4()
+
+# Define endline to create a new room
+@app.post("/rooms/create_room",
+          response_model=RoomOut,
+          status_code=status.HTTP_201_CREATED)
+async def create_room(new_room: RoomIn) -> RoomOut:
+    for room in ROOMS:
+        if room["room_name"] == new_room.room_name:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room name already exists")
+        if new_room.players_expected < 2 or new_room.players_expected > 4:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong amount of players")
+
+    try:
+        last_id = ROOMS[-1]['room_id'] if ROOMS else 0
+        new_id = last_id + 1
+
+        # Create a new room dict
+        roomOut = RoomOut(room_id=new_id,
+                          room_name=new_room.room_name,
+                          players_expected=new_room.players_expected,
+                          owner_name=new_room.owner_name)
+    
+        ROOMS.append(roomOut.model_dump())
+        
+        await manager.broadcast("Game created")        
+        return roomOut.model_dump()
+    
     except Exception as e:
         print(f"Error: {e}")  # Debug error
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
+# Define endpoint to get rooms list
+@app.get("/rooms/")
+async def get_rooms():
+    try:
+        return ROOMS
+    except Exception as e:
+        print(f"Error: {e}")  # Debug error
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+    
+# endpoint for join room
+@app.put("/rooms/join/")
+def join_room_endpoint(room_id: int, player_name: str):
+    try:
+        room = get_room_by_id(room_id)
+
+        if room is None:
+            return {"message": "Room not found"}
+
+        if len(room["players"]) == room["players_expected"]:
+            return {"message": "Room is full"}
+        
+        if player_name in room["players"]:
+            return {"message": "The name already exists, choose another"}
+        
+        room["players"].append(player_name)
+        return {"message": f"The player {player_name} has joined the room {room_id}"}
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
+# endpoint for room join request 
+@app.put("/rooms/leave/")
+def leave_room_endpoint(room_id: int, player_name: str):
+    try:
+        room = get_room_by_id(room_id)
+
+        if room == None:
+            return {"message": "Room not found"}
+
+        if not(player_name in room["players"]):
+            return {"message": "There is not such a player"}
+        
+        room["players"].remove(player_name)
+        return {"message": f"The player {player_name} has left the room {room_id}"}
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error") 
