f9f4c69031d2204a1e486587c1bb0a0ccac9b427
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2024-09-18 01:49:56 -0300

    SES-11 added tests and modified room struct

--- app/test_get_room.py
+++ app/test_get_room.py
@@ -1,104 +1,37 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-from room import rooms 
+from room import *
 from typing import List
 
 from main import app
 
 client = TestClient(app)
 
-def test_basic():
-    room_name = "Room 1"
-    players_expected = 2
-    players: List[str] = []
-
-    new_room_data = {
-        "room_name": room_name,
-        "players_expected": players_expected
-    }
-
-    sent = client.post("/rooms/", json= new_room_data)
-    assert sent.status_code == status.HTTP_201_CREATED
-
-    response = client.get("/rooms/")
-    assert response.status_code == status.HTTP_200_OK
-
-    rooms_data = response.json()
-
-    assert len(rooms_data) == 1
-    assert rooms_data == new_room_data
-
-def test_big_loop():
-    i = 1
-    while (i < 100):
-        room_name = f"Room {i}"
-        players_expected = 4
-        new_room_data = {"room_name": room_name, 
-                         "players_expected": players_expected}
-
-        sent = client.post("/rooms/", json= new_room_data)
-        assert sent.status_code == status.HTTP_201_CREATED
-
-        response = client.get("/rooms/")
-        assert response.status_code == status.HTTP_200_OK
-
-        rooms_data = response.json()
-        assert rooms_data[-1] == new_room_data
-        i += 1
-        
-def test_weird():
-    room_name = "@!#./^"
-    players_expected = 4
-    new_room_data = {"room_name": room_name,
-                 "players_expected": players_expected}
-
-    sent = client.post("/rooms/", json= new_room_data)
-    assert sent.status_code == status.HTTP_201_CREATED
+def test_empty_rooms():
     
     response = client.get("/rooms/")    
     assert response.status_code == status.HTTP_200_OK
 
-    rooms_data = response.json()
+    assert response.json() == []
 
-    assert rooms_data[-1] == new_room_data
+def test_basic_get():
 
-def test_same_name():
-    room_name1 = "Room 1"
-    players_expected1 = 2
+    room_name = "Room 1"
+    players_expected = 2
+    owner = "carlitos"
 
-    room_name2 = "Room 1"
-    players_expected2 = 2
-
-    # rooms now have extra data
-    new_room_data1 = {
-        "room_name": room_name1,
-        "players_expected": players_expected1
-    }
+    new_room_data = RoomIn(
+        room_name= room_name,
+        players_expected = players_expected,
+        owner_name = owner
+    )
     
-    new_room_data2 = {
-        "room_name": room_name2,
-        "players_expected": players_expected2
-    }
-
-    sent1 = client.post("/rooms/", json= new_room_data1)
-    assert sent1.status_code == status.HTTP_201_CREATED
-
-    sent2 = client.post("/rooms/", json= new_room_data2)
-    assert sent2.status_code == status.HTTP_201_CREATED
+    sent = client.post("/rooms/create_room", json= new_room_data.model_dump() )
+    assert sent.status_code == status.HTTP_201_CREATED
+    # Check if room is correctly stored
+    assert sent.json() in ROOMS
 
     response = client.get("/rooms/")
     assert response.status_code == status.HTTP_200_OK
-
-    rooms_data = response.json()
-
-    assert rooms_data[-2]['room_name'] == room_name1
-    assert rooms_data[-2]['players_expected'] == players_expected1
-    assert rooms_data[-1]['room_name'] == room_name2
-    assert rooms_data[-1]['players_expected'] == players_expected2
-
-def test_large_get():
-    response = client.get("rooms")
-    assert response.status_code == status.HTTP_200_OK
-
-    rooms_data = response.json()
-    assert rooms_data == rooms.ROOMS
+    # Check if room is correctly 
+    assert response.json() not in ROOMS
