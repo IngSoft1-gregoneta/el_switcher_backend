0f79b8bb297d4fc3be00d9a0274c1f2db925fd22
Facundo Mourelle <38925289+FacuMou@users.noreply.github.com>
2024-09-18 22:02:31 -0300

    Review: fixed wrong path run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Establecer la ruta base del proyecto
-PROJECT_DIR="/home/tadeofiorini/Documents/la-gregoneta/el_switcher_backend/app"
-
 # Ejecutar pytest sobre todos los archivos en el directorio tests
-PYTHONPATH=$PROJECT_DIR pytest -s $PROJECT_DIR/tests --disable-warnings
+PYTHONPATH=app pytest -s app/tests --disable-warnings
