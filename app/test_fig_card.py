6e36b85e615f1ca83fa6f093fd7c5fe325e9df50
yamil <yamil.ali@mi.unc.edu.ar>
2024-09-19 19:21:50 -0300

    test_fig_card actualized

--- app/test_fig_card.py
+++ app/test_fig_card.py
@@ -9,7 +9,6 @@ def test_valid_fig_card():
         FigCard(match_id=1, player_name="Player1", card_color=CardColor.WHITE, fig_type=random.choice(white_figs))
         FigCard(match_id=1, player_name="Player1", card_color=CardColor.BLUE, fig_type=random.choice(blue_figs))
     except ValueError as e:
-        print(f"Error: {e}")
         assert False, f"Error: {e}"
 
 def test_valid_fig_card_str():
@@ -17,8 +16,7 @@ def test_valid_fig_card_str():
         FigCard(match_id=1, player_name="Player1", card_color="White", fig_type="L")
         FigCard(match_id=1, player_name="Player1", card_color="Blue", fig_type="B1")
     except ValueError as e:
-        print(f"Error: {e}")
-        assert False
+        assert False, f"Error: {e}"
 
 def test_bad_color_fig_card():
     try:
