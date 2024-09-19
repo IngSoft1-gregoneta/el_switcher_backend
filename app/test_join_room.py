08ea457ec30dda1c80927aa05267b4585b1aebaf
yamil <yamil.ali@mi.unc.edu.ar>
2024-09-19 19:48:56 -0300

    feature_SES-13 adding return of a RoomOut model for join_room, and tests

--- app/test_join_room.py
+++ app/test_join_room.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-from rooms import *
+from room import *
 from main import app
 
 client = TestClient(app)
@@ -8,12 +8,12 @@ client = TestClient(app)
 def test_room():
     ROOMS.clear()
     room_name = "Room 1"
-    players_expected = 2
+    players_expected = 3
     owner_name = "Yamil"
     roomOut = RoomOut(room_id=1,
                           room_name=room_name,
                           players_expected=players_expected,
-                          players_names=[],
+                          players_names=[owner_name],
                           owner_name=owner_name,
                           is_active=True)
     ROOMS.append(roomOut.model_dump())
@@ -22,14 +22,20 @@ def test_room():
 def test_join_room1():
     test_room()
     room_id = 1
-    player_name = "Yamil"
+    player_name = "Tito"
     
-    expected_response = {"message": f"The player {player_name} has joined the room {room_id}"}
+    expected_response = {"room_id": 1,
+                         "room_name": "Room 1",
+                         "players_expected": 3,
+                         "players_names": ["Yamil", "Tito"],
+                         "owner_name": "Yamil",
+                         "is_active": True
+                         }
     response = client.put(f"/rooms/join?room_id={room_id}&player_name={player_name}")
     
-    assert ROOMS[0]["players_names"] == ["Yamil"]
+    assert ROOMS[0]["players_names"] == ["Yamil", "Tito"]
     assert ROOMS[0]["room_id"] == 1
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_202_ACCEPTED
     assert response.json() == expected_response
 
 # test para que otro jugador se una a la misma partida
@@ -37,12 +43,18 @@ def test_join_room2():
     room_id = 1 
     player_name = "Tadeo"
     
-    expected_response = {"message": f"The player {player_name} has joined the room {room_id}"}
+    expected_response = {"room_id": 1,
+                         "room_name": "Room 1",
+                         "players_expected": 3,
+                         "players_names": ["Yamil", "Tito", "Tadeo"],
+                         "owner_name": "Yamil",
+                         "is_active": True
+                         }
     response = client.put(f"/rooms/join?room_id={room_id}&player_name={player_name}")
     
-    assert ROOMS[0]["players_names"] == ["Yamil", "Tadeo"]
+    assert ROOMS[0]["players_names"] == ["Yamil","Tito","Tadeo"]
     assert ROOMS[0]["room_id"] == 1
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_202_ACCEPTED
     assert response.json() == expected_response
 
 # test para evitar que otro jugador se una a una sala llena
@@ -54,16 +66,15 @@ def test_join_full_room():
     
     response = client.put(f"/rooms/join?room_id={room_id}&player_name={player_name}")
     
-    assert ROOMS[0]["players_names"] == ["Yamil", "Tadeo"] # deberia seguir siendo esta la lista
+    assert ROOMS[0]["players_names"] == ["Yamil", "Tito", "Tadeo"] # deberia seguir siendo esta la lista
     assert ROOMS[0]["room_id"] == 1
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_202_ACCEPTED
     assert response.json() == expected_response
 
 # test para asegurarse de que no haya duplicados de nombres de jugadores
 def test_same_name():
     test_room()
     room_id = 1
-    ROOMS[0]["players_names"] = ["Yamil"]  # simular que el jugador ya está en la sala
     
     player_name = "Yamil"
     expected_response = {"message": "The name already exists, choose another"}
@@ -71,5 +82,5 @@ def test_same_name():
     
     assert ROOMS[0]["players_names"] == ["Yamil"] # no deberia dejar unir a otro jugador con el mismo nombre
     assert ROOMS[0]["room_id"] == 1
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_202_ACCEPTED
     assert response.json() == expected_response
