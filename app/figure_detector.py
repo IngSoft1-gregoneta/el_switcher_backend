444f4e735aab3773996cd70373f874ab3f9ccd3b
tadeofiorini <tadeo.nino.039@mi.unc.edu.ar>
2024-10-06 01:17:28 -0300

    fige02 ok

--- app/figure_detector.py
+++ app/figure_detector.py
@@ -26,6 +26,7 @@ def figures_detector(match: MatchOut):
     for y in range(columns):
         for x in range(columns):
             match_out = fige01_detector(match_out, x, y)
+            match_out = fige02_detector(match_out, x, y)
             match_out = fige03_detector(match_out, x, y)
             match_out = fige05_detector(match_out, x, y)
             match_out = fige07_detector(match_out, x, y)
@@ -82,6 +83,29 @@ def fige01_rot2_detector(match: MatchOut, x: int, y: int) -> MatchOut:
         
 def fige01_rot2_verifications(center_x: int, center_y: int):
     return center_y+1<columns and center_y-1>=0 and center_x+1<columns
+
+def fige02_detector(match: MatchOut, x: int, y: int) -> MatchOut:
+    center_x = x
+    center_y = y
+    match_out = match
+    if match is None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="match not found")
+    if (fige02_verifications(center_x, center_y)):
+        color = match_out.board.tiles[coordinates_to_index(x, y)].tile_color
+        up = (center_x, center_y-1)
+        up_right = (center_x+1, center_y-1)
+        right = (center_x+1, center_y)
+        if match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(up_right[0], up_right[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_color == color:
+            match_out.board.tiles[coordinates_to_index(center_x, center_y)].tile_in_figure = FigType.fige02.value
+            match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_in_figure = FigType.fige02.value
+            match_out.board.tiles[coordinates_to_index(up_right[0], up_right[1])].tile_in_figure = FigType.fige02.value
+            match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_in_figure = FigType.fige02.value
+    return match_out
+
+def fige02_verifications(center_x: int, center_y: int):
+    return center_y-1>=0 and center_x+1<columns
 
 def fige03_detector(match: MatchOut, x: int, y: int) -> MatchOut:
     match_out = match
--- app/tests/test_figure_detector.py
+++ app/tests/test_figure_detector.py
@@ -93,6 +93,30 @@ def test_fige01_rot_2_detector():
     assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige01.value
     assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 2)].tile_in_figure == FigType.fige01.value
 
+def test_fige02_detector():
+    reset()
+    generate_test_room()
+    generate_test_match()
+
+    match = repo_match.get_match_by_id(room_id)
+    for x in range(figure_detector.columns):
+        for y in range(figure_detector.columns):
+            match.board.tiles[figure_detector.coordinates_to_index(x, y)].tile_color = TileColor.RED.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 0)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_color = TileColor.BLUE.value
+    repo_match.update_match(match)
+
+    match = repo_match.get_match_by_id(room_id)
+    figure_detector.figures_detector(match)
+    match_out = repo_match.get_match_by_id(room_id)
+    
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 0)].tile_in_figure == FigType.fige02.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_in_figure == FigType.fige02.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_in_figure == FigType.fige02.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige02.value
+
 def test_fige03_rot_1_detector():
     reset()
     generate_test_room()
