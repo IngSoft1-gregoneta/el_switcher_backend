43a2382aa83fe734e4b8d07373b7a2dba1fdc2f6
tadeofiorini <tadeo.nino.039@mi.unc.edu.ar>
2024-10-06 12:57:43 -0300

    fige04 ok

--- app/figure_detector.py
+++ app/figure_detector.py
@@ -43,6 +43,8 @@ def figures_detector(match: MatchOut):
                 match_out = fige02_detector(match_out, x, y)
             if FigType.fige03.value in fig_types:
                 match_out = fige03_detector(match_out, x, y)
+            if FigType.fige04.value in fig_types:
+                match_out = fige04_detector(match_out, x, y)
             if FigType.fige05.value in fig_types:
                 match_out = fige05_detector(match_out, x, y)
             if FigType.fige07.value in fig_types:
@@ -175,6 +177,107 @@ def fige03_rot2_detector(match: MatchOut, x: int, y: int) -> MatchOut:
         
 def fige03_rot2_verifications(center_x: int, center_y: int):
     return center_y+1<columns and center_y-1>=0 and center_x+1<columns
+
+
+def fige04_detector(match: MatchOut, x: int, y: int) -> MatchOut:
+    match_out = match
+    match_out = fige04_rot1_detector(match_out, x, y)
+    match_out = fige04_rot2_detector(match_out, x, y)
+    match_out = fige04_rot3_detector(match_out, x, y)
+    match_out = fige04_rot4_detector(match_out, x, y)
+    return match_out
+
+def fige04_rot1_detector(match: MatchOut, x: int, y: int) -> MatchOut:
+    center_x = x
+    center_y = y
+    match_out = match
+    if match is None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="match not found")
+    if (fige04_rot1_verifications(center_x, center_y)):
+        color = match_out.board.tiles[coordinates_to_index(x, y)].tile_color
+        up = (center_x, center_y-1)
+        left = (center_x-1, center_y)
+        right = (center_x+1, center_y)
+        if match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_color == color:
+            match_out.board.tiles[coordinates_to_index(center_x, center_y)].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_in_figure = FigType.fige04.value
+    return match_out
+
+def fige04_rot1_verifications(center_x: int, center_y: int):
+    return center_y-1>=0 and center_x+1<columns and center_x-1>=0
+
+def fige04_rot2_detector(match: MatchOut, x: int, y: int) -> MatchOut:
+    center_x = x
+    center_y = y
+    match_out = match
+    if match is None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="match not found")
+    if (fige04_rot2_verifications(center_x, center_y)):
+        color = match_out.board.tiles[coordinates_to_index(x, y)].tile_color
+        up = (center_x-1, center_y)
+        left = (center_x, center_y+1)
+        right = (center_x, center_y-1)
+        if match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_color == color:
+            match_out.board.tiles[coordinates_to_index(center_x, center_y)].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_in_figure = FigType.fige04.value
+    return match_out
+        
+def fige04_rot2_verifications(center_x: int, center_y: int):
+    return center_y+1<columns and center_y-1>=0 and center_x-1>=0
+
+def fige04_rot3_detector(match: MatchOut, x: int, y: int) -> MatchOut:
+    center_x = x
+    center_y = y
+    match_out = match
+    if match is None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="match not found")
+    if (fige04_rot3_verifications(center_x, center_y)):
+        color = match_out.board.tiles[coordinates_to_index(x, y)].tile_color
+        up = (center_x, center_y+1)
+        left = (center_x+1, center_y)
+        right = (center_x-1, center_y)
+        if match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_color == color:
+            match_out.board.tiles[coordinates_to_index(center_x, center_y)].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_in_figure = FigType.fige04.value
+    return match_out
+
+def fige04_rot3_verifications(center_x: int, center_y: int):
+    return center_y+1<columns and center_x+1<columns and center_x-1>=0
+
+def fige04_rot4_detector(match: MatchOut, x: int, y: int) -> MatchOut:
+    center_x = x
+    center_y = y
+    match_out = match
+    if match is None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="match not found")
+    if (fige04_rot4_verifications(center_x, center_y)):
+        color = match_out.board.tiles[coordinates_to_index(x, y)].tile_color
+        up = (center_x+1, center_y)
+        left = (center_x, center_y-1)
+        right = (center_x, center_y+1)
+        if match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_color == color and \
+        match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_color == color:
+            match_out.board.tiles[coordinates_to_index(center_x, center_y)].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(up[0], up[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(left[0], left[1])].tile_in_figure = FigType.fige04.value
+            match_out.board.tiles[coordinates_to_index(right[0], right[1])].tile_in_figure = FigType.fige04.value
+    return match_out
+        
+def fige04_rot4_verifications(center_x: int, center_y: int):
+    return center_y-1>=0 and center_y+1<columns and center_x+1<columns
 
 
 def fige05_detector(match: MatchOut, x: int, y: int) -> MatchOut:
--- app/tests/test_figure_detector.py
+++ app/tests/test_figure_detector.py
@@ -199,6 +199,108 @@ def test_fige03_rot_2_detector():
     assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige03.value
     assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_in_figure == FigType.fige03.value
 
+def test_fige04_rot_1_detector():
+    reset()
+    generate_test_room()
+    generate_test_match()
+
+    match = repo_match.get_match_by_id(room_id)
+    match.players[0].fig_cards[0].fig_type = 'fige04'
+    for x in range(figure_detector.columns):
+        for y in range(figure_detector.columns):
+            match.board.tiles[figure_detector.coordinates_to_index(x, y)].tile_color = TileColor.RED.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(2, 1)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_color = TileColor.BLUE.value
+    repo_match.update_match(match)
+
+    match = repo_match.get_match_by_id(room_id)
+    figure_detector.figures_detector(match)
+    match_out = repo_match.get_match_by_id(room_id)
+    
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(2, 1)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_in_figure == FigType.fige04.value
+
+def test_fige04_rot_2_detector():
+    reset()
+    generate_test_room()
+    generate_test_match()
+
+    match = repo_match.get_match_by_id(room_id)
+    match.players[0].fig_cards[0].fig_type = 'fige04'
+    for x in range(figure_detector.columns):
+        for y in range(figure_detector.columns):
+            match.board.tiles[figure_detector.coordinates_to_index(x, y)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 2)].tile_color = TileColor.RED.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_color = TileColor.RED.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_color = TileColor.RED.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_color = TileColor.RED.value
+    repo_match.update_match(match)
+
+    match = repo_match.get_match_by_id(room_id)
+    figure_detector.figures_detector(match)
+    match_out = repo_match.get_match_by_id(room_id)
+    
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 2)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_in_figure == FigType.fige04.value
+
+def test_fige04_rot_3_detector():
+    reset()
+    generate_test_room()
+    generate_test_match()
+
+    match = repo_match.get_match_by_id(room_id)
+    match.players[0].fig_cards[0].fig_type = 'fige04'
+    for x in range(figure_detector.columns):
+        for y in range(figure_detector.columns):
+            match.board.tiles[figure_detector.coordinates_to_index(x, y)].tile_color = TileColor.BLUE.value
+    match.board.tiles[figure_detector.coordinates_to_index(2, 0)].tile_color = TileColor.YELLOW.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_color = TileColor.YELLOW.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 0)].tile_color = TileColor.YELLOW.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_color = TileColor.YELLOW.value
+    repo_match.update_match(match)
+
+    match = repo_match.get_match_by_id(room_id)
+    figure_detector.figures_detector(match)
+    match_out = repo_match.get_match_by_id(room_id)
+    
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(2, 0)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 0)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 0)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige04.value
+
+def test_fige04_rot_4_detector():
+    reset()
+    generate_test_room()
+    generate_test_match()
+
+    match = repo_match.get_match_by_id(room_id)
+    match.players[0].fig_cards[0].fig_type = 'fige04'
+    for x in range(figure_detector.columns):
+        for y in range(figure_detector.columns):
+            match.board.tiles[figure_detector.coordinates_to_index(x, y)].tile_color = TileColor.RED.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 0)].tile_color = TileColor.GREEN.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_color = TileColor.GREEN.value
+    match.board.tiles[figure_detector.coordinates_to_index(0, 2)].tile_color = TileColor.GREEN.value
+    match.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_color = TileColor.GREEN.value
+    repo_match.update_match(match)
+
+    match = repo_match.get_match_by_id(room_id)
+    figure_detector.figures_detector(match)
+    match_out = repo_match.get_match_by_id(room_id)
+    
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 0)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 1)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(0, 2)].tile_in_figure == FigType.fige04.value
+    assert match_out.board.tiles[figure_detector.coordinates_to_index(1, 1)].tile_in_figure == FigType.fige04.value
+
+    reset()
+
 
 def test_fige05_rot_1_detector():
     reset()
