ac9c279537f3266f7dd3d57650f1fe269f2afa6c
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2024-10-07 19:17:19 -0300

    SES-95 moved to match-handler

--- app/match_handler.py
+++ app/match_handler.py
@@ -95,9 +95,17 @@ class MatchHandler:
             detail="Internal Server Error",
         )
 
-    async def check_winner(self, match_id: UUID) -> Union[str, None]:
+    async def check_winner(self, match_id: UUID):
         try:
-            winner = self.repo.check_winner(match_id)
+            match = self.repo.get_match_by_id(match_id)
+            if match is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Match not found")
+            
+            winner = None
+            for player in match.players:
+                # No tiene mas cartas de figura
+                if len(player.fig_cards) == 0:
+                    winner = player.player_name
             return winner
         except Exception as e:
             if isinstance(e, HTTPException):
