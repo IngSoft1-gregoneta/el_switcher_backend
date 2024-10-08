6c4075038ce1d7494afc83127b90377243682754
tadeofiorini <tadeo.nino.039@mi.unc.edu.ar>
2024-10-07 21:38:26 -0300

    fig09 ok

--- app/figure_detector.py
+++ app/figure_detector.py
@@ -1,7 +1,7 @@
 from app.models.visible_match import VisibleMatchData
 from models.match import * 
 from figures import fige01, fige02, fige03, fige04, fige05, fige06, fige07, \
-    fig01, fig02, fig03, fig04, fig05, fig06, fig07, fig08
+    fig01, fig02, fig03, fig04, fig05, fig06, fig07, fig08, fig09
 # primero vamos a hacer las figuras blancas
 match_repo = MatchRepository()
 columns = int(AMOUNT_OF_TILES ** 0.5)
@@ -44,6 +44,7 @@ def figures_detector(match: MatchOut):
             detect_fig06(match_out, fig_types, x, y)
             detect_fig07(match_out, fig_types, x, y)
             detect_fig08(match_out, fig_types, x, y)
+            detect_fig09(match_out, fig_types, x, y)
     match_repo.update_match(match_out)
 
 def get_valid_fig_types(match: MatchOut) -> List[str]:
@@ -115,3 +116,7 @@ def detect_fig07(match: MatchOut, fig_types: List[FigType], x: int, y: int) -> M
 def detect_fig08(match: MatchOut, fig_types: List[FigType], x: int, y: int) -> MatchOut:
     if FigType.fig08.value in fig_types:
         match = fig08.fig08_detector(match, x, y)
+
+def detect_fig09(match: MatchOut, fig_types: List[FigType], x: int, y: int) -> MatchOut:
+    if FigType.fig09.value in fig_types:
+        match = fig09.fig09_detector(match, x, y)
