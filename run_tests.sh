bcbbc713382b12664684ee82d506e79e15343b1f
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2024-10-15 20:41:01 -0300

    SES-128 añadido fix al script + dotfile para omitir tests en report de coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-PYTHONPATH=app coverage run -m pytest --disable-warnings|| true
+PYTHONPATH=app coverage run --rcfile=.coveragerc -m pytest --disable-warnings|| true
 PYTHONPATH=app coverage report -m
